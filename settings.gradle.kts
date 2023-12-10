81faec8aa2343558b475ed84a073e42f83ce1ca8
Nikita <n.klimkin99@yandex.ru>
2023-12-10 16:35:27 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,5 @@ include(":expenses-manager:domain")
 include(":common")
 include("expenses-manager:use-case")
 include("expenses-manager:persistence")
-findProject(":expenses-manager:persistence")?.name = "persistence"
 include("expenses-manager:rest")
-findProject(":expenses-manager:rest")?.name = "rest"
 include("expenses-manager:application")
-findProject(":expenses-manager:application")?.name = "application"
